b753b523c46f9cc6f4d1bdab7db15774d0eb5844
OTube <69583657+OTube@users.noreply.github.com>
2020-09-13 12:16:11 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,9 @@
+var point = localStorage.getItem("pointvalue")
+  		document.getElementById("pt").innerHTML = point;
+  		alert(point);
+		function increaser()
+		{
+			point++;
+			document.getElementById("pt").innerHTML = point;
+			localStorage.setItem("pointvalue", point);
+		}
